315daaba4f29107f4b14ce8704af92a84dc0f966
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-10-22 14:40:44 -0400

    updated hours, and picture

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,20 @@ function Home() {
     return (
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
-                <h1 class="display-4">Welcome to Billy Bob's Gud Fud and Other Eatables</h1>
-                <p class="lead">Located at 28887 Jethro Way behind the haircare and tire center.</p>
-                <p>With up to the second pricing!</p>
+                <div class="row">
+                    <div class="col-6">
+                        <h1 class="display-4">Welcome to Billy Bob's Gud Fud and Other Ateables</h1>
+                        <p class="lead">Located behind the haircare and tire center.</p>
+                        <ul>
+                            <li>Open: Sun-up</li>
+                            <li>Close: Sun-down</li>
+                        </ul>
+                        <p class="lead">Pricing depends on what was caught today!</p>
+                    </div>
+                    <div class="col-6 d-flex align-items-center">
+                        <img class="img-fluid max-width: 100% height: auto" src="./img/tom-martin-hillbilly.jpg" ></img>
+                    </div>
+                </div>
             </div>
         </div>
     )
